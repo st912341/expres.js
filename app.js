52a91bac5878c7218028bd4caad20a1374e98f83
st912341 <71476272+st912341@users.noreply.github.com>
2023-08-21 10:28:14 +0530

    Url-handling

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 //const http = require("http");
 const express = require("express");
 const app = express();
+const bodyPraser=require('body-parser');
+app.use(bodyPraser.urlencoded());
 
-app.use((req, res, next) => {
+app.use('/add-product',(req, res, next) => {
     console.log("In the middleware");
-    next();
+    //res.send('<h2>Add product page </h2>');
+    res.send('<form action="/product" method="POST">Name : <input type="text" name ="title"> <button type="submit"> Add product </> </form>')
+    //next();
 });
-app.use((req, res, next) => {
+app.use("/product",(req,res,next)=>{
+ console.log(req.body);
+ res.redirect('/add-product');
+})
+app.use('/',(req, res, next) => {
     console.log("In the another middleware");
     res.send('<h2>hello sourabh</h2>');
     //next();
